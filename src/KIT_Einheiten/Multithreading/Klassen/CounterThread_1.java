bd1dface4065474c9a20a7535e5633806a4b358f
Adrian Weidig <70036819+adrianweidig@users.noreply.github.com>
2023-03-23 15:03:19 +0100

    Docs Anpassung
    Anpassung der Docs Quelle für GitHub pages

--- src/KIT_Einheiten/Multithreading/Klassen/CounterThread_1.java
+++ src/KIT_Einheiten/Multithreading/Klassen/CounterThread_1.java
@@ -5,6 +5,18 @@ public class CounterThread_1 implements Runnable {
     public void run() {
         for (int i = 0; i < 10000; i++) {
             System.out.println("CounterThread 1 - " + i);
+
+            // "Bremse"
+            // for (long l = 0; l < 100000; l++);
+
+            // Auch "Bremse"
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
+
+        System.out.println("CounterThread 1 beendet.");
     }
 }
--- src/KIT_Einheiten/Multithreading/Klassen/CounterThread_2.java
+++ src/KIT_Einheiten/Multithreading/Klassen/CounterThread_2.java
@@ -6,5 +6,7 @@ public class CounterThread_2 implements Runnable{
         for (int i = 0; i < 10000; i++) {
             System.err.println("CounterThread 2 - " + i);
         }
+
+        System.err.println("CounterThread 2 beendet.");
     }
 }
